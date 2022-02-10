1b6db5bf7bdb494828abbbd89e5c24b5d544e61d
DeusFerrariis <cleghornw@protonmail.com>
2022-02-09 18:04:27 -0700

    init

---
+++ src/routes/__error.svelte
@@ -0,0 +1,30 @@
+<script context="module">
+	/** @type {import('@sveltejs/kit').ErrorLoad} */
+	export function load({ error, status }) {
+		return {
+			props: {
+				title: `${status}: ${error.message}`,
+				status: status
+			}
+		};
+	}
+</script>
+
+<script>
+	export let title;
+	export let status;
+</script>
+
+{#if (status == 404)}
+  <h1>Nothing to see here :/</h1>
+{/if}
+
+<style>
+  h1 {
+    font-family: 'Inter', sans-serif;
+    font-size: 1.4rem;
+    padding: 0;
+    margin: 0;
+    color: white;
+  }
+</style>
