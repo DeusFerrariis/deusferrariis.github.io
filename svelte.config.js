67d53a638d1727ae20ab296e86f4f45dd99d6db9
DeusFerrariis <cleghornw@protonmail.com>
2022-02-09 18:31:20 -0700

    added support for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const dev = "production" === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
-	kit: {
-		adapter: adapter()
-	}
+    preprocess: preprocess(),
+    kit: {
+        adapter: adapter({
+            pages: "docs",
+            assets: "docs"
+        }),
+        paths: {
+            // change below to your repo name
+            base: dev ? "" : "/deusferrariis.github.io",
+        },
+    }
 };
 
 export default config;
