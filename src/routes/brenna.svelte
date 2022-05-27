1373e4a5897ba8c851920e3aa1f4632808260806
DeusFerrariis <cleghornw@protonmail.com>
2022-05-27 13:13:08 -0600

    brenna page

---
+++ src/routes/brenna.svelte
@@ -0,0 +1,26 @@
+<img src="rupert_stego.png" style="height: 33vh;" alt="Rupert"/>
+<p>
+	Download Me :) ^
+</p>
+<p>
+	<a href="https://en.wikipedia.org/wiki/Steganography">What are least significant bits(LSB)?</a>
+</p>
+
+<style>
+	.center {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+	}
+	a {
+    font-family: 'Inter', sans-serif;
+		color: white;
+	}
+
+	p {
+    font-family: 'Inter', sans-serif;
+		color: white;
+		font-size: 12px;
+	}
+</style>
