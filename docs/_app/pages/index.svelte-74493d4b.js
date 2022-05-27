7069b3e31ecb50e98ac8aee0b6d2fcec2ce51459
DeusFerrariis <cleghornw@protonmail.com>
2022-05-27 13:20:16 -0600

    fixed index.svelte

---
+++ docs/_app/pages/index.svelte-74493d4b.js
@@ -0,0 +1,1 @@
+import{S as t,i as e,s as o}from"../chunks/vendor-e8c164e4.js";class n extends t{constructor(s){super();e(this,s,null,null,o,{})}}export{n as default};
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,2 @@
 <script>
-    import hljs from 'highlight.js';
-    hljs.highlightAll();
 </script>
-
-<pre><code class="language-go">fmt.Println("Hello World")</code></pre>
