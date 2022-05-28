29f2045af4b3621d8f809d2b2fdd871954c0ebce
DeusFerrariis <cleghornw@protonmail.com>
2022-05-28 13:56:49 -0600

    brenna pt 2

--- src/routes/brenna.svelte
+++ src/routes/brenna.svelte
@@ -1,11 +1,10 @@
-<img src="rupert_stego.png" style="height: 33vh;" alt="Rupert"/>
+<img src="https://imgs.search.brave.com/c0G2WTDcBGlzXjPbem1vqmrOGWo0IsZNU-AzRu_Fwe8/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvM2I5/ZWMwZTktMjA5Mi00/MWY5LTk4MzUtMjZk/YTA0ZDVhYWJlLjVm/NTY2ZjkxYzkwNWNm/ZmU0MTJmMWViMjUw/NGQxYzk0LmpwZWc"/>
 <p>
-	Download Me :) ^
+	Key: 7, oaawz://whzalipu.jvt/ngqAnb2u
 </p>
 <p>
-	<a href="https://en.wikipedia.org/wiki/Steganography">What are least significant bits(LSB)?</a>
+	Key: 20, jqx: lojyln
 </p>
-
 <style>
 	.center {
 		display: flex;
@@ -22,5 +21,8 @@
     font-family: 'Inter', sans-serif;
 		color: white;
 		font-size: 12px;
+	}
+	img {
+		width: 33%;
 	}
 </style>
